d58d71ba9453b0c0d4af1e364a07db0ab86b14b8
Gabriel Maciel <rmbiel30@gmail.com>
2024-08-25 21:09:45 -0300

    Adicionada lógica com JS a tela de autenticação

---
+++ index.js
@@ -0,0 +1,65 @@
+function onChangeEmail() {
+    toglleButtonsDisabled();
+    toglleEmailErrors();
+   
+}
+function onChangePassword(){
+   toglleButtonsDisabled(); 
+   togllePasswordErrors();
+    
+}
+function isEmailValid() {
+    const email = document.getElementById("email").value;
+    if (!email){
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function toglleEmailErrors() {
+    const email = document.getElementById('email').value;
+    if (!email) {
+        document.getElementById('email-required-error').style.display = "block";
+    }
+    else {
+        document.getElementById('email-required-error').style.display = "none";
+    }
+
+    if (validateEmail(email)) {
+        document.getElementById('email-invalid-error').style.display = "none";
+    }
+    else {
+        document.getElementById('email-invalid-error').style.display = "block";
+    }
+}
+
+function togllePasswordErrors() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        document.getElementById('password-required-error').style.display = "block";
+    }
+    else {
+        document.getElementById('password-required-error').style.display = "none";
+    }
+
+}
+
+function toglleButtonsDisabled(){
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+}
+
+function isPasswordValid() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+}
