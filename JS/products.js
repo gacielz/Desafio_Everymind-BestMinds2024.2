8142f0f8f1deb162fd9831fb0413d6e39f76481b
Gabriel Maciel <rmbiel30@gmail.com>
2024-08-28 22:34:59 -0300

    Adicionada validação a tela de adição de produtos

--- JS/products.js
+++ JS/products.js
@@ -23,3 +23,41 @@ function formatPrice() {
         }
     });
 }
+
+function onChangeName() {
+    const name = form.name().value;
+    form.nameRequiredError().style.display = !name ? "block" : "none";
+    toggleSaveButtonType()
+}
+
+function onChangeDesc() {
+    const desc = form.desc().value;
+    form.descRequiredError().style.display = !desc ? "block" : "none";
+    toggleSaveButtonType()
+}
+
+function toggleSaveButtonType() {
+    form.saveButton().disabled = !isFormValid();
+}
+
+function isFormValid() {
+    const name = form.name().value;
+    if (!name) {
+        return false;
+    }
+
+    const desc = form.desc().value;
+    if (!desc) {
+        return false;
+    }
+
+    return true;
+}
+
+const form = {
+    name: () => document.getElementById('name'),
+    nameRequiredError: () => document.getElementById('name-required-error'),
+    desc: () => document.getElementById('desc'),
+    descRequiredError: () => document.getElementById('desc-required-error'),
+    saveButton: () => document.getElementById('save-button')
+}
